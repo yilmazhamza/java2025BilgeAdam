8324ecc2f445be1e9738e0688bc66564047aa0a8
Hamza Yılmaz <95347617+yilmazhamza@users.noreply.github.com>
2024-11-10 10:01:51 +0300

    ödev
    ödev

---
+++ Bilgeadam2025/src/Bilgeadam2025.java
@@ -0,0 +1,5 @@
+public class Bilgeadam2025 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ cumartesi09112024/src/IfStatement.java
@@ -0,0 +1,18 @@
+public class IfStatement {
+    public static void main(String[] args) {
+
+        //09.11.2024 ödev
+        int number1 = 7;
+        int number2 = 13;
+
+        if(number1 < number2){
+            System.out.println("1. sayı  2. sayıdan küçüktür.");
+        }
+        else if (number1 == number2){
+            System.out.println("bu iki sayı birbirine eşittir.");
+        }
+        else {
+            System.out.println("1. sayı 2. sayıdan büyüktür.");
+        }
+    }
+}
